3eea2f9ee4b0dab8fa033cce50c95a8c52c08dc6
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-04-07 22:13:03 -0500

    Update JobTracker.java

--- PasswordCracker/JobTracker.java
+++ PasswordCracker/JobTracker.java
@@ -9,7 +9,8 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class JobTracker {
+public class JobTracker 
+{
 
     String myPath = "/jobtracker";
     String pendingPath = "/pending";
